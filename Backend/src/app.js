70af103fb25892b1cd8ff34d8ec357c5ce6dfb7f
Your Name <deepesh3950lodhi@gmail.com>
2026-07-05 23:47:38 +0530

    Shopnest done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -31,7 +31,7 @@ app.get('/' ,(req,res) => {
 })
 app.use(express.static("/public"))
 app.use("*name", (req, res) => {
-    res.sendFile(path.join(__dirname,"..","/public/index/html"))
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
 });
 
 export default app

73e6d9d6f2a12cb16a687edf9f4c6b47414e0ca0
Your Name <deepesh3950lodhi@gmail.com>
2026-07-06 00:05:38 +0530

    Shopnest

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,44 +1,47 @@
-import express, { urlencoded } from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import path from 'path'
-import { fileURLToPath } from 'url'
-// Router import 
-import authRouter from './routes/authRoutes.js'
-import  productRouter  from './routes/productRoutes.js'
-import orderRouter from './routes/orderRoutes.js'
-import paymentRouter from './routes/paymentRoutes.js'
-import analyticsRouter from './routes/analyticsRoutes.js'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// Routes
+import authRouter from "./routes/authRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import paymentRouter from "./routes/paymentRoutes.js";
+import analyticsRouter from "./routes/analyticsRoutes.js";
 
-// Create an instance of express
-const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// middlewares
-app.use(express.json())
-app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
-}))
-app.use(cookieParser())
-app.use(express.urlencoded({extended: true}))
-// routes
-app.use('/api/auth' , authRouter)
-app.use('/api/products' , productRouter)
-app.use('/api/orders', orderRouter)
-app.use('/api/payment' , paymentRouter)
-app.use('/api/analytics' , analyticsRouter)
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
-app.get('/' ,(req,res) => {
-    res.send("Helllooo world")
-})
-app.use(express.static("/public"))
+// API Routes
+app.use("/api/auth", authRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/payment", paymentRouter);
+app.use("/api/analytics", analyticsRouter);
 
-app.use("*name", (req, res) => {
-    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+// Serve React build
+app.use(express.static(path.join(__dirname, "../public")));
+
+// React Router
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-export default app
+export default app;
